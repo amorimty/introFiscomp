5c2726ee4a81a27d3dd8e24cbeebfcc12b09b323
amorimty <lcamendonca@usp.br>
2025-11-12 18:28:45 -0300

    checkpoint em casa

--- projeto_5/exerA.f90
+++ projeto_5/exerA.f90
@@ -1,8 +1,8 @@
 program exerA
     implicit none
     
-    real(8) :: r,x, x_prev
-    integer(8) :: i = 0
+    real(8) :: r,x, x_prev, temp = 0.0_8
+    integer :: i = 0, n = 1000
 
     ! r = 3.2_8
     ! x = 0.8_8
@@ -10,17 +10,25 @@ program exerA
     read(*,*) r
     read(*,*) x
 
-    open(unit=1, file='map.dat', status='replace', action='write')
+    open(unit=1, file='map_linha.dat', status='replace', action='write')
+    open(unit=2, file='map_pontos.dat', status='replace', action='write')
 
-    do i = 1, 1000, 1
+    
+    write(1, *) x, 0.0_8
+
+    do i = 1, n, 1
         x_prev = x
         x = r*x*(1.0_8 - x)
         ! write(1,*) i, x
+        write(2,*) x_prev, x
         write(1,*) x_prev, x
-
+        write(1, *) x, x
+        
+        if (i == n) print*, 'x* ~', x
     end do
 
     close(1)  
+    close(2)
 
 end program exerA
 
