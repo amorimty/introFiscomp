1f29aaad6cf36c5cd1cd22dee683b3662c49a930
Lucas Campos Amorim de Mendonça <lcamendonca@usp.br>
2025-11-13 10:26:57 -0300

    tentei fazer a linearizacao da exponencial

--- projeto_5/exerB.f90
+++ projeto_5/exerB.f90
@@ -1,8 +1,10 @@
 program exerB
     implicit none
     
-    real(8) :: r,x, eps, s, x2=0.0_8, lambda, der
-    integer(8) :: i = 0, n=1000, ntemp=100
+    real(8) :: r,x, eps, s, x2=0.0_8, lambda, der, d, dsat=0.15_8
+    real(8) :: lambdaMinimos
+    real(8) :: sum_i=0.0_8, sum_y=0.0_8, sum_ii=0.0_8, sum_iy=0.0_8
+    integer(8) :: i = 0, n=1000, ntemp=100, count=0
 
     ! r = 3.95_8
     ! x = 0.8_8
@@ -17,9 +19,12 @@ program exerB
     s = 0.0_8
 
     do i = 1, n
+        ! Iteracao das orbitas
         x = r*x*(1.0_8 - x)
         x2 = r*x2*(1.0_8 - x2)
+        d = abs(x2 - x)
 
+        ! Calculo do expoente de Lyapunov
         if(i>ntemp) then
             der = r*(1.0_8 - 2.0_8*x)
 
@@ -29,13 +34,26 @@ program exerB
             
             s = s + log(abs(der))
         end if
-        write(1,*)i, x, abs(x-x2)
-        ! write(1,*)i, abs(x-x2)
-        
+
+        ! write(1,*)i, x, d
+        write(1,*)i, d
+
+        ! Soma do minimos quadrados
+        if (d < dsat) then
+            sum_i  = sum_i  + real(i,8)
+            sum_y  = sum_y  + log(d)
+            sum_ii = sum_ii + real(i,8)**2
+            sum_iy = sum_iy + real(i,8)*log(d)
+            count  = count + 1
+        end if
+
     end do
 
+    lambdaMinimos = (count*sum_iy - sum_i*sum_y) / (count*sum_ii - sum_i*sum_i)
+
     lambda = s/real(n-ntemp,8)
-    write(*,*) 'Lyapunov Exponent = ', lambda
+    write(*,*) 'Lyapunov Exponent Formula = ', lambda
+    write(*,*) 'Lyapunov Exponent Minimos Quadrados = ', lambdaMinimos
 
     close(1)  
 
